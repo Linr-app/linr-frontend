fb55b34d92d90aa8c663d28ccf24bd3dd4a93e44
Alex Alves <alexlalves@live.com>
2017-10-30 12:24:14 -0200

    Changed CalculateHash to global CreateSnowflake function

--- src/components/Entrarnafila.vue
+++ src/components/Entrarnafila.vue
@@ -3,23 +3,21 @@
     <div class="content">
       <section class="hero">
         <div class="hero-body">
-          <button class="button is-large is-primary"
-                  v-on:click="entrar_na_fila">ENTRAR NA FILA
-          </button>
-          <br/>
+          <button class="button is-large is-primary" v-on:click="entrar_na_fila">ENTRAR NA FILA</button><br/>
         </div>
+      </section>
     </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'Entrarnafila',
-    methods: {
-      entrar_na_fila: function (event) {
-        var cljs = new ClientJS()
-        alert('Entrou na fila! ' + cljs.getFingerprint())
-        console.log(event)
-      },
-    },
-  }
+    export default {
+        name: 'Entrarnafila',
+        methods: {
+            entrar_na_fila: function(event) {
+                alert(this.$CalculateSnowflake(0, 0));
+                console.log(event)
+            }
+        }
+    }
 </script>
